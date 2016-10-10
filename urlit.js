4517e52647b01bee37bc1c436a236886efe75724
Ludvig Böklin <sudoreboot@protonmail.com>
2016-10-10 16:54:49 +0200

    Fix regexp to remove the GET requests

--- urlit.js
+++ urlit.js
@@ -2,6 +2,6 @@ var pageurl = window.location.href;
 pageurl = pageurl.replace(/http.*\:\/*/, '');
 pageurl = pageurl.replace(/www./, '');
 pageurl = pageurl.replace(/(.*)\/$/, "$1");
-pageurl = pageurl.replace(/\/\?.*/, '');
+pageurl = pageurl.replace(/\?.*/, '');
 
 window.document.title = pageurl;
